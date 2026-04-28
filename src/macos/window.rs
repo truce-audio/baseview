373c9a5b6f2ec1ecde68394c99adb5709cd51108
Mahae Koh <flomatiks@gmail.com>
2026-04-28 16:21:04 -0700

    Implement cursor.rs for macos

--- src/macos/window.rs
+++ src/macos/window.rs
@@ -369,8 +369,8 @@ impl<'a> Window<'a> {
         }
     }
 
-    pub fn set_mouse_cursor(&mut self, _mouse_cursor: MouseCursor) {
-        todo!()
+    pub fn set_mouse_cursor(&mut self, mouse_cursor: MouseCursor) {
+        unsafe { super::cursor::set_cursor(mouse_cursor) };
     }
 
     #[cfg(feature = "opengl")]
